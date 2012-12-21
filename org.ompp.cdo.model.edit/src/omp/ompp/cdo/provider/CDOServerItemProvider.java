d046c5cc063ef3d7297f0cebe20ba4aa666c74b0
gcolburn <gcolburn@gmail.com>
2012-12-20 19:57:07 -0800

    Switching to EMF model for CDO Client and Server for storing preferences

---
+++ org.ompp.cdo.model.edit/src/omp/ompp/cdo/provider/CDOServerItemProvider.java
@@ -0,0 +1,205 @@
+/**
+ */
+package omp.ompp.cdo.provider;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import omp.ompp.cdo.CDOServer;
+import omp.ompp.cdo.OMPPCdoPackage;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+/**
+ * This is the item provider adapter for a {@link omp.ompp.cdo.CDOServer} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class CDOServerItemProvider
+	extends ItemProviderAdapter
+	implements
+		IEditingDomainItemProvider,
+		IStructuredItemContentProvider,
+		ITreeItemContentProvider,
+		IItemLabelProvider,
+		IItemPropertySource {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public CDOServerItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addPortPropertyDescriptor(object);
+			addRepositoryNamePropertyDescriptor(object);
+			addResourceNamePropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Port feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addPortPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_CDOServer_port_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_CDOServer_port_feature", "_UI_CDOServer_type"),
+				 OMPPCdoPackage.Literals.CDO_SERVER__PORT,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Repository Name feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addRepositoryNamePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_CDOServer_repositoryName_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_CDOServer_repositoryName_feature", "_UI_CDOServer_type"),
+				 OMPPCdoPackage.Literals.CDO_SERVER__REPOSITORY_NAME,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Resource Name feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addResourceNamePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_CDOServer_resourceName_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_CDOServer_resourceName_feature", "_UI_CDOServer_type"),
+				 OMPPCdoPackage.Literals.CDO_SERVER__RESOURCE_NAME,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This returns CDOServer.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/CDOServer"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		String label = ((CDOServer)object).getRepositoryName();
+		return label == null || label.length() == 0 ?
+			getString("_UI_CDOServer_type") :
+			getString("_UI_CDOServer_type") + " " + label;
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(CDOServer.class)) {
+			case OMPPCdoPackage.CDO_SERVER__PORT:
+			case OMPPCdoPackage.CDO_SERVER__REPOSITORY_NAME:
+			case OMPPCdoPackage.CDO_SERVER__RESOURCE_NAME:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getResourceLocator() {
+		return OmppCDOServerEditPlugin.INSTANCE;
+	}
+
+}
---
+++ org.ompp.cdo.model/src/omp/ompp/cdo/CDOClient.java
@@ -0,0 +1,159 @@
+/**
+ */
+package omp.ompp.cdo;
+
+import org.eclipse.emf.cdo.CDOObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>CDO Client</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link omp.ompp.cdo.CDOClient#getName <em>Name</em>}</li>
+ *   <li>{@link omp.ompp.cdo.CDOClient#getDescription <em>Description</em>}</li>
+ *   <li>{@link omp.ompp.cdo.CDOClient#getUri <em>Uri</em>}</li>
+ *   <li>{@link omp.ompp.cdo.CDOClient#getRepositoryName <em>Repository Name</em>}</li>
+ *   <li>{@link omp.ompp.cdo.CDOClient#getResourceName <em>Resource Name</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see omp.ompp.cdo.OMPPCdoPackage#getCDOClient()
+ * @model
+ * @extends CDOObject
+ * @generated
+ */
+public interface CDOClient extends CDOObject {
+	/**
+	 * Returns the value of the '<em><b>Name</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Name</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Name</em>' attribute.
+	 * @see #setName(String)
+	 * @see omp.ompp.cdo.OMPPCdoPackage#getCDOClient_Name()
+	 * @model
+	 * @generated
+	 */
+	String getName();
+
+	/**
+	 * Sets the value of the '{@link omp.ompp.cdo.CDOClient#getName <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Name</em>' attribute.
+	 * @see #getName()
+	 * @generated
+	 */
+	void setName(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Description</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Description</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Description</em>' attribute.
+	 * @see #setDescription(String)
+	 * @see omp.ompp.cdo.OMPPCdoPackage#getCDOClient_Description()
+	 * @model
+	 * @generated
+	 */
+	String getDescription();
+
+	/**
+	 * Sets the value of the '{@link omp.ompp.cdo.CDOClient#getDescription <em>Description</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Description</em>' attribute.
+	 * @see #getDescription()
+	 * @generated
+	 */
+	void setDescription(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Uri</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Uri</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Uri</em>' attribute.
+	 * @see #setUri(String)
+	 * @see omp.ompp.cdo.OMPPCdoPackage#getCDOClient_Uri()
+	 * @model
+	 * @generated
+	 */
+	String getUri();
+
+	/**
+	 * Sets the value of the '{@link omp.ompp.cdo.CDOClient#getUri <em>Uri</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Uri</em>' attribute.
+	 * @see #getUri()
+	 * @generated
+	 */
+	void setUri(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Repository Name</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Repository Name</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Repository Name</em>' attribute.
+	 * @see #setRepositoryName(String)
+	 * @see omp.ompp.cdo.OMPPCdoPackage#getCDOClient_RepositoryName()
+	 * @model
+	 * @generated
+	 */
+	String getRepositoryName();
+
+	/**
+	 * Sets the value of the '{@link omp.ompp.cdo.CDOClient#getRepositoryName <em>Repository Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Repository Name</em>' attribute.
+	 * @see #getRepositoryName()
+	 * @generated
+	 */
+	void setRepositoryName(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Resource Name</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Resource Name</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Resource Name</em>' attribute.
+	 * @see #setResourceName(String)
+	 * @see omp.ompp.cdo.OMPPCdoPackage#getCDOClient_ResourceName()
+	 * @model
+	 * @generated
+	 */
+	String getResourceName();
+
+	/**
+	 * Sets the value of the '{@link omp.ompp.cdo.CDOClient#getResourceName <em>Resource Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Resource Name</em>' attribute.
+	 * @see #getResourceName()
+	 * @generated
+	 */
+	void setResourceName(String value);
+
+} // CDOClient
